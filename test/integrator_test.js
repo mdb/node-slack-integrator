2ab2ac032eee62f7a463e68830fe9e730dd5735b
Mike Ball <mikedball@gmail.com>
2015-01-25 16:53:49 -0500

    add integrator tests

--- test/integrator_test.js
+++ test/integrator_test.js
@@ -1,31 +1,52 @@
 var assert = require('assert'),
     request = require('supertest'),
-    Integrator = require('../integrator');
-
-function inst() {
-  return new Integrator({
-    handler: function (req, res, next) {
-      // your custom handler
-      return res.status(204).end();
-    },
-    webHookPath: 'foo'
-  });
-}
-
-var server = inst();
+    nock = require('nock'),
+    Integrator = require('../integrator'),
+    inst = (function () {
+      return new Integrator({
+        payload: function (req, res) {
+          return {
+            some: 'payload'
+          };
+        },
+        hookPath: 'foo'
+      });
+    })();
 
 describe('GET /', function() {
   it('returns "Hello world!"', function(done) {
-    request(server.app)
-    .get('/')
-    .expect(200, 'Hello world!', done);
+    request(inst.app)
+      .get('/')
+      .expect(200, 'Hello world!', done);
   });
 });
 
 describe('POST /integration', function() {
-  it('returns 204', function(done) {
-    request(server.app)
-    .post('/integration')
-    .expect(204, done);
+  describe('when the post to the slack integration endpoint succeeds', function () {
+    var scope = nock('https://hooks.slack.com')
+                  .post('/services/foo', {
+                    some: 'payload'
+                  })
+                  .reply(200);
+
+    it('returns 200 and includes the payload as its response body', function(done) {
+      request(inst.app)
+        .post('/integration')
+        .expect(200, {some: 'payload'}, done);
+    });
+  });
+
+  describe('when the post to the slack integration endpoint fails', function () {
+    var scope = nock('https://hooks.slack.com')
+                  .post('/services/foo', {
+                    some: 'payload'
+                  })
+                  .reply(400);
+
+    it('returns 400', function(done) {
+      request(inst.app)
+        .post('/integration')
+        .expect(400, done);
+    });
   });
 });
