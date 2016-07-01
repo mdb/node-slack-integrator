b687483c81c519db6ba4a1801b2c87fa7e9219fc
Mike Ball <mikedball@gmail.com>
2016-07-01 07:39:29 -0400

    use es2015 features

--- integrator.js
+++ integrator.js
@@ -1,66 +1,74 @@
-var express = require('express'),
-    request = require('request'),
-    bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 
-function Integrator(config) {
-  var port = process.env.PORT || config.port || 3000,
-      token = config.token,
-      hookUrl = 'https://hooks.slack.com/services/' + config.hookPath;
+class Integrator {
+  constructor(config) {
+    this.config = Object.assign({
+      token: process.env.SLACK_TOKEN,
+      port: process.env.PORT || 3000,
+      hookPath: undefined
+    }, config);
 
-  this.app = express();
-  this.app.use(bodyParser.urlencoded({ extended: true }));
-  this.app.use(bodyParser.json());
+    this.config.hookUrl = 'https://hooks.slack.com/services/' + config.hookPath;
 
-  // test route
-  this.app.get('/', function (req, res) {
-    res.status(200).send('Hello world!');
-  });
+    this.app = express();
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
 
-  this.app.post('/integration', handleReq);
+    // test route
+    this.app.get('/', (req, res) => {
+      res.status(200).send('Hello world!');
+    });
 
-  // error handler
-  this.app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(400).send(err.message);
-  });
+    this.app.post('/integration', (req, res, next) => {
+      this._handleReq(req, res, next);
+    });
 
-  this.app.listen(port, function () {
-    console.log('Integration service listening on port ' + port);
-  });
+    // error handler
+    this.app.use(function (err, req, res, next) {
+      console.error(err.stack);
+      res.status(400).send(err.message);
+    });
 
-  function handleReq(req, res, next) {
-    if (token && req.body && req.body.token !== token) {
-      return responseWithCode(401, res);
+    this.app.listen(this.config.port, () => {
+      console.log('Integration service listening on port ' + this.config.port);
+    });
+  }
+
+  _handleReq(req, res, next) {
+    if (this.config.token && req.body && req.body.token !== this.config.token) {
+      return this._responseWithCode(401, res);
     }
 
-    config.payload(req, function (payload) {
-      sendPayload(payload, function (error, status, body) {
+    this.config.payload(req, (payload) => {
+      this._sendPayload(payload, (error, status, body) => {
         if (error) {
           return next(error);
         } else if (status >= 400) {
           return next(new Error('Incoming WebHook: ' + status + ' ' + body));
         } else {
-          return responseWithCode(200, res, payload);
+          return this._responseWithCode(200, res, payload);
         }
       });
     });
   }
 
-  function responseWithCode(code, res, payload) {
+  _responseWithCode(code, res, payload) {
     payload = payload || '';
 
     return res
-            .status(code)
-            .send((config.debug ? payload : ''))
-            .end();
+      .status(code)
+      .send((this.config.debug ? payload : ''))
+      .end();
   }
 
-  function sendPayload(payload, callback) {
+  _sendPayload(payload, callback) {
     request({
-      uri: hookUrl,
+      uri: this.config.hookUrl,
       method: 'POST',
       body: JSON.stringify(payload)
-    }, function (error, response, body) {
+    }, (error, response, body) => {
       if (error) { return callback(error); }
 
       callback(null, response.statusCode, body);
